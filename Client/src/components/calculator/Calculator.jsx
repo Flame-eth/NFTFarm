151e7bfe78f92e2f6b9a9e1d2d9d7f30dc692b22
Flame-eth <theflamepen@gmail.com>
2023-06-28 18:56:17 +0100

    yeild calculator

--- Client/src/components/calculator/Calculator.jsx
+++ Client/src/components/calculator/Calculator.jsx
@@ -10,46 +10,60 @@ const YieldCalculator = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     const yieldAmount = (amountInvested * duration * yieldPercentage) / 100;
+    console.log(yieldAmount);
     setResult(yieldAmount.toFixed(2));
   };
 
   return (
     <div className="yield-calculator">
+      <h1>Yield Calculator</h1>
+      <p>
+        Calculate the yield amount you will earn on your investment in a
+        particular duration.
+      </p>
+
       <form onSubmit={handleSubmit}>
         <label>
-          Amount Invested:
+          Investment Amount
           <input
             type="number"
             value={amountInvested}
+            placeholder="e.g 1000"
             onChange={(e) => setAmountInvested(e.target.value)}
           />
         </label>
         <br />
         <label>
-          Duration (in years):
+          Duration (in days)
           <input
             type="number"
             value={duration}
+            placeholder="e.g 4"
             onChange={(e) => setDuration(e.target.value)}
           />
         </label>
         <br />
         <label>
-          Yield Percentage:
+          Yield Percentage
           <input
             type="number"
             value={yieldPercentage}
+            placeholder="e.g 1.7"
             onChange={(e) => setYieldPercentage(e.target.value)}
           />
         </label>
-        <br />
-        <button type="submit">Calculate</button>
       </form>
       <br />
-      <label>
-        Result:
-        <input type="text" value={result} readOnly />
-      </label>
+      <button onClick={handleSubmit} type="submit">
+        Calculate
+      </button>
+      <br />
+      <div className="result">
+        <label>
+          Earning
+          <input type="text" value={result} readOnly />
+        </label>
+      </div>
     </div>
   );
 };
