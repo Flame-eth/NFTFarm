1bb880899276f447fc65e52b55d4fd499d76e0f3
Flame-eth <theflamepen@gmail.com>
2023-06-29 18:56:58 +0100

    responsiveness

--- Client/src/components/navbar/Navbar.jsx
+++ Client/src/components/navbar/Navbar.jsx
@@ -7,54 +7,18 @@ import { IoClose } from "react-icons/io5";
 const Navbar = () => {
   const [sidebar, setSidebar] = useState(false);
   return (
-    <div className="navbar">
-      <div className="navContainer">
-        <div className="navIcon">
-          <img src="" alt="" />
-          <h1>
-            <Link className="navLink" to="/">
-              YieldNFT
-            </Link>
-          </h1>
-        </div>
-        <div className="navLinks">
-          <ul>
-            <li>
+    <div className="Container">
+      <div className="navbar">
+        <div className="navContainer">
+          <div className="navIcon">
+            <img src="" alt="" />
+            <h1>
               <Link className="navLink" to="/">
-                Home
+                YieldNFT
               </Link>
-            </li>
-            {/* <li>
-              <Link className="navLink" to="/marketplace">
-                Marketplace
-              </Link>
-            </li> */}
-            <li>
-              <Link className="navLink" to="/account">
-                Account
-              </Link>
-            </li>
-            <li>
-              <Link className="navLink" to="/referral">
-                Referral
-              </Link>
-            </li>
-            {/* <li>
-              <Link className="navLink" to="/faq">
-                FAQ
-              </Link>
-            </li> */}
-          </ul>
-        </div>
-        <div className="sidebar">
-          <div className="sidebarIcon">
-            {sidebar ? (
-              <IoClose onClick={() => setSidebar(!sidebar)} />
-            ) : (
-              <GiHamburgerMenu onClick={() => setSidebar(!sidebar)} />
-            )}
+            </h1>
           </div>
-          {sidebar ? (
+          <div className="navLinks">
             <ul>
               <li>
                 <Link className="navLink" to="/">
@@ -82,11 +46,51 @@ const Navbar = () => {
               </Link>
             </li> */}
             </ul>
-          ) : (
-            ""
-          )}
+          </div>
+          <div className="sidebar">
+            <div className="sidebarIcon">
+              {sidebar ? (
+                <IoClose onClick={() => setSidebar(!sidebar)} />
+              ) : (
+                <GiHamburgerMenu onClick={() => setSidebar(!sidebar)} />
+              )}
+            </div>
+          </div>
         </div>
       </div>
+      {sidebar ? (
+        <div className="sideLinks">
+          <ul>
+            <li>
+              <Link className="sideLink" to="/">
+                Home
+              </Link>
+            </li>
+            {/* <li>
+              <Link className="navLink" to="/marketplace">
+                Marketplace
+              </Link>
+            </li> */}
+            <li>
+              <Link className="sideLink" to="/account">
+                Account
+              </Link>
+            </li>
+            <li>
+              <Link className="sideLink" to="/referral">
+                Referral
+              </Link>
+            </li>
+            {/* <li>
+              <Link className="navLink" to="/faq">
+              FAQ
+              </Link>
+            </li> */}
+          </ul>
+        </div>
+      ) : (
+        ""
+      )}
     </div>
   );
 };
