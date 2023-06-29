e660b10c7bf582b80eec9f95ee2b4ef08f460207
Flame-eth <theflamepen@gmail.com>
2023-06-29 18:00:49 +0100

    responsiveness

--- Client/src/components/navbar/Navbar.jsx
+++ Client/src/components/navbar/Navbar.jsx
@@ -1,7 +1,11 @@
 import { Link } from "react-router-dom";
 import "./Navbar.scss";
+import { useState } from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { IoClose } from "react-icons/io5";
 
 const Navbar = () => {
+  const [sidebar, setSidebar] = useState(false);
   return (
     <div className="navbar">
       <div className="navContainer">
@@ -41,6 +45,46 @@ const Navbar = () => {
               </Link>
             </li> */}
           </ul>
+        </div>
+        <div className="sidebar">
+          <div className="sidebarIcon">
+            {sidebar ? (
+              <IoClose onClick={() => setSidebar(!sidebar)} />
+            ) : (
+              <GiHamburgerMenu onClick={() => setSidebar(!sidebar)} />
+            )}
+          </div>
+          {sidebar ? (
+            <ul>
+              <li>
+                <Link className="navLink" to="/">
+                  Home
+                </Link>
+              </li>
+              {/* <li>
+              <Link className="navLink" to="/marketplace">
+                Marketplace
+              </Link>
+            </li> */}
+              <li>
+                <Link className="navLink" to="/account">
+                  Account
+                </Link>
+              </li>
+              <li>
+                <Link className="navLink" to="/referral">
+                  Referral
+                </Link>
+              </li>
+              {/* <li>
+              <Link className="navLink" to="/faq">
+                FAQ
+              </Link>
+            </li> */}
+            </ul>
+          ) : (
+            ""
+          )}
         </div>
       </div>
     </div>
