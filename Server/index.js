2587212d847afbbc7bcc5ff3b6fc62ae26bac2e8
Flame-eth <theflamepen@gmail.com>
2023-07-03 12:18:30 +0100

    ackend init

---
+++ Server/index.js
@@ -0,0 +1,52 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+const app = express();
+dotenv.config();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.use((err, req, res, next) => {
+  // res.header("Access-Control-Allow-Origin", "*");
+  // res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  // res.header("Access-Control-Allow-Headers", "Content-Type");
+
+  // res.setHeader("Access-Control-Allow-Origin", "*");
+  // res.setHeader("Access-Control-Allow-Credentials", "true");
+  // res.setHeader("Access-Control-Max-Age", "1800");
+  // res.setHeader("Access-Control-Allow-Headers", "content-type");
+  // res.setHeader(
+  //   "Access-Control-Allow-Methods",
+  //   "PUT, POST, GET, DELETE, PATCH, OPTIONS"
+  // );
+
+  // Enabling CORS
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,OPTIONS,POST,PUT, DELETE"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization"
+  );
+
+  const errorStatus = err.status || 500;
+  const errorMessage = err.message || "Something went wrong";
+  res.status(errorStatus).json({
+    success: false,
+    status: errorStatus,
+    message: errorMessage,
+    stack: err.stack,
+  });
+
+  next();
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
