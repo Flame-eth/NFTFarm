6657ad08a8c738fc79514408899d61951173e99f
Flame-eth <theflamepen@gmail.com>
2023-07-21 09:56:55 +0100

    origin

--- Server/index.js
+++ Server/index.js
@@ -30,31 +30,39 @@ app.use(
   // }
 );
 app.use(express.json());
+app.use("/api/users", userRoute);
+app.use("/api/transactions", transactionRoute);
+
+cron.schedule("* * * * *", () => {
+  // Task to be executed
+  updateBalance();
+  console.log("Cron job running...");
+});
 
 app.use((err, req, res, next) => {
   // res.header("Access-Control-Allow-Origin", "*");
   // res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
   // res.header("Access-Control-Allow-Headers", "Content-Type");
 
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader("Access-Control-Max-Age", "1800");
-  res.setHeader("Access-Control-Allow-Headers", "content-type");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "PUT, POST, GET, DELETE, PATCH, OPTIONS"
-  );
+  // res.setHeader("Access-Control-Allow-Origin", "*");
+  // res.setHeader("Access-Control-Allow-Credentials", "true");
+  // res.setHeader("Access-Control-Max-Age", "1800");
+  // res.setHeader("Access-Control-Allow-Headers", "content-type");
+  // res.setHeader(
+  //   "Access-Control-Allow-Methods",
+  //   "PUT, POST, GET, DELETE, PATCH, OPTIONS"
+  // );
 
   // Enabling CORS
-  // res.header("Access-Control-Allow-Origin", "*");
-  // res.header(
-  //   "Access-Control-Allow-Methods",
-  //   "GET,HEAD,OPTIONS,POST,PUT, DELETE"
-  // );
-  // res.header(
-  //   "Access-Control-Allow-Headers",
-  //   "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization"
-  // );
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,OPTIONS,POST,PUT, DELETE"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization"
+  );
 
   const errorStatus = err.status || 500;
   const errorMessage = err.message || "Something went wrong";
@@ -66,15 +74,6 @@ app.use((err, req, res, next) => {
   });
 
   next();
-});
-
-app.use("/api/users", userRoute);
-app.use("/api/transactions", transactionRoute);
-
-cron.schedule("* * * * *", () => {
-  // Task to be executed
-  updateBalance();
-  console.log("Cron job running...");
 });
 
 connectDB().then(() => {
